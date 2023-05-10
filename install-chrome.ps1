24a74e641ff9cf9ba2ffaebe6daf95720dc448ba
Blake Wallace <bwallace@3-gis.com>
2023-05-10 10:16:08 -0400

    final-version

--- install-chrome.ps1
+++ install-chrome.ps1
@@ -1,4 +1,9 @@
-$installer = $env:TEMP + "\chrome_installer.exe";
-Invoke-WebRequest "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $installer;
-Start-Process -FilePath $installer -Args "/silent/install" -Verb RunAs -Wait;
+#Set location for chrome installation package
+$installer = "$HOME\downloads" + "\chrome_installer.exe";
+
+#Download installation file and run with arguments
+(new-object System.Net.WebClient).DownloadFile('https://dl.google.com/chrome/install/latest/chrome_installer.exe', "$installer"); 
+& "$installer" /silent /install;
+
+#Remove chrome installer after finished
 Remove-Item $installer;
